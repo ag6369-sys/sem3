b078c3caaaf61822f023c4cb15b51a1ad35092d5
ARYA G A <ag6369@srmist.edu.in>
2026-09-19 00:10:50 +0530

    Add Session 3 Product.java

---
+++ src/main/java/oop/assigment_problems/Product.java
@@ -0,0 +1,16 @@
+package oop.assigment_problems;
+
+public class Product {
+    String productId;
+    String productName;
+
+    public Product(String productId, String productName) {
+        this.productId = productId;
+        this.productName = productName;
+    }
+
+    public static void main(String[] args) {
+        Product product = new Product("P-1042", "Wireless Mouse");
+        System.out.println(product.productId + " - " + product.productName);
+    }
+}
