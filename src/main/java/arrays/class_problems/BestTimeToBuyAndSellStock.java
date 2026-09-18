463c0df221a5c4820b9af90b2c84f107bf8cfb21
ARYA G A <ag6369@srmist.edu.in>
2026-09-19 00:11:11 +0530

    Add Session 4 BestTimeToBuyAndSellStock.java

---
+++ src/main/java/arrays/class_problems/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,17 @@
+package arrays.class_problems;
+
+public class BestTimeToBuyAndSellStock {
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) minPrice = prices[i];
+            maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+}
