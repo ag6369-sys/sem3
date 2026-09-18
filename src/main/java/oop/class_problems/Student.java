34375c3453c2652f849438caa6ea39d073e7f801
ARYA G A <ag6369@srmist.edu.in>
2026-09-19 00:10:30 +0530

    Add Session 3 Student.java

---
+++ src/main/java/oop/class_problems/Student.java
@@ -0,0 +1,13 @@
+package oop.class_problems;
+
+public class Student {
+    String name;
+    int marks;
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.name = "Divya";
+        student.marks = 88;
+        System.out.println("Name: " + student.name + " | Marks: " + student.marks);
+    }
+}
