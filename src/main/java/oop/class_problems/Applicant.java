d33773726612490a35d7749e0d05b38c45e62036
ARYA G A <ag6369@srmist.edu.in>
2026-09-19 00:10:40 +0530

    Add Session 3 Applicant.java

---
+++ src/main/java/oop/class_problems/Applicant.java
@@ -0,0 +1,16 @@
+package oop.class_problems;
+
+public class Applicant {
+    static int totalApplicants = 0;
+
+    public Applicant() {
+        totalApplicants++;
+    }
+
+    public static void main(String[] args) {
+        new Applicant();
+        new Applicant();
+        new Applicant();
+        System.out.println("Total applicants: " + Applicant.totalApplicants);
+    }
+}
