92567b136b46b221359d1fddec39b07b8270ca77
ARYA G A <ag6369@srmist.edu.in>
2026-09-19 00:12:33 +0530

    Add Session 5 TopPerformerTracker.java

---
+++ src/main/java/arrays_and_methods/assigment_problems/TopPerformerTracker.java
@@ -0,0 +1,16 @@
+package arrays_and_methods.assigment_problems;
+
+public class TopPerformerTracker {
+    public static String findMinMaxSpread(int[] scores) {
+        int min = scores[0], max = scores[0];
+        for (int score : scores) {
+            if (score < min) min = score;
+            if (score > max) max = score;
+        }
+        return "Min: " + min + " | Max: " + max + " | Spread: " + (max - min);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMinMaxSpread(new int[]{45, 82, 79, 90, 33, 90, 61}));
+    }
+}
